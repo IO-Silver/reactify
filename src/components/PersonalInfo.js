4a52bf0733e050824c9fad3ee519e754aef621aa
Rio Silver <rjcsilver@gmail.com>
2024-07-10 11:20:00 -0400

    Fleshing out Personal Info

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react'
 
 class PersonalInfo extends Component {
   render(props) {
-    return <h1>{props.full_name}</h1>
+    return <div>
+            <h1>{props.full_name}</h1>
+            <p>
+              {props.degree} <br>
+              {props.email} <br>
+              {props.phone}
+            </p>
+          </div>
+
   }
 }
 
